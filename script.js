89300488e1dd8f9cd8e00d5af360d6e881711153
aidar <a1dargti@gmail.com>
2024-08-14 13:08:28 +0500

    print array works

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 const myLibrary = [
   {
     title: "Lord of the Rings",
-    author: "Tolkien",
+    author: "Mark Tolkien",
     pages: 500,
     read: true,
   },
   {
     title: "Aquarium",
-    author: "Viktor",
+    author: "Viktor Suvorov",
     pages: 258,
     read: true,
   },
 ];
 
-myLibrary.forEach(function (fruit) {
-  console.log(fruit);
-});
+// var p = document.createElement("p");
+// document.body.appendChild(p);
 
-const card = document.createElement("div");
-const title = document.createElement("div");
-const author = document.createElement("div");
-const pages = document.createElement("div");
-
-title.textContent = "hey";
+const wrapper = document.querySelector(".wrapper");
+for (let i = 0; i < myLibrary.length; i++) {
+  const card = document.createElement("div");
+  card.className = "card";
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const pages = document.createElement("div");
+  title.textContent = myLibrary[i].title;
+  author.textContent = myLibrary[i].author;
+  pages.textContent = myLibrary[i].pages;
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  wrapper.appendChild(card);
+}
 
 function Book() {
   // the constructor...
